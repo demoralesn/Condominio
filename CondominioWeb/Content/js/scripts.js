0c1a6971b6e96d3a545d34ef75bfcc7f14dfcc55
Daniel Morales <dmorales.sahid@mafchile.com>
2019-12-31 12:45:09 -0300

    31/12

--- CondominioWeb/Content/js/scripts.js
+++ CondominioWeb/Content/js/scripts.js
@@ -55,3 +55,34 @@ var DeleteRealState = function () {
         }
     })
 }
+
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector(".list-group");
+
+button.addEventListener("click", function () {
+    var li = document.createElement("li");
+    var inp = document.createElement("input")
+    var div = document.createElement("div")
+    var btn = document.createElement("a")
+    var btni = document.createElement("i")
+
+    li.className = 'list-group-item';
+    inp.className = 'form-control';
+    div.className = 'col-lg-2';
+    btn.className = 'btn btn-danger btn-circle btn-sm';
+    btni.className = 'fas fa-trash';
+
+    btn.setAttribute('href', "#");
+
+    li.style.fontWeight = "bold";
+
+    li.textContent = input.value;
+
+    btn.innerHTML += btni.outerHTML;
+
+    div.innerHTML += 'Cantidad: ' + inp.outerHTML + 'Monto: ' + inp.outerHTML + ' ' + btn.outerHTML;
+
+    li.innerHTML += div.outerHTML;
+    ul.appendChild(li);
+})
