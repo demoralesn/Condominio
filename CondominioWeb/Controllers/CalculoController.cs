131eed278bc06f7f285b2d9b6b08117a4a059b9e
Daniel Morales <demoralesn@gmail.com>
2020-01-06 00:52:45 -0300

    06-01

--- CondominioWeb/Controllers/CalculoController.cs
+++ CondominioWeb/Controllers/CalculoController.cs
@@ -14,35 +14,8 @@ namespace CondominioWeb.Controllers
         // GET: Calculo
         public ActionResult Index()
         {
-            var pro = CargarTipos();
-
-            IEnumerable<SelectListItem> Tipos = pro.Select(x => new SelectListItem
-            {
-                Value = x.Id.ToString(),
-                Text = x.Descripcion
-            });
-
-            ViewBag.Tipos = Tipos;
-
             return View();
-        }
-
-        private List<ListCalculo> CargarTipos()
-        {
-            var tiposList = new List<ListCalculo>();
-            using (var dt = BaseDatos.ExecuteDataTable("sp_c_tipo_calculo"))
-            {
-                foreach (DataRow row in dt.Rows)
-                {
-                    var tipos = new ListCalculo()
-                    {
-                        Id = (int)row["id"],
-                        Descripcion = row["descripcion"].ToString(),
-                    };
-                    tiposList.Add(tipos);
-                }
-            }
-            return tiposList;
+            
         }
     }
 }
