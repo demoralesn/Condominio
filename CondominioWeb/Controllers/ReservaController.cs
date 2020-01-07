a7f3abb387584d98ba13ccd000d4e8766427972e
Daniel Morales <demoralesn@gmail.com>
2020-01-07 00:57:21 -0300

    07/11

--- CondominioWeb/Controllers/ReservaController.cs
+++ CondominioWeb/Controllers/ReservaController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CondominioWeb.Models;
+using CondominioWeb.DAL;
+using System.Data;
 
 namespace CondominioWeb.Controllers
 {
@@ -11,7 +14,66 @@ namespace CondominioWeb.Controllers
         // GET: Reserva
         public ActionResult Index()
         {
+            var pro = CargarGrilla();
+
+            IEnumerable<SelectListItem> Prop = pro.Select(x => new SelectListItem
+            {
+                Value = x.Id.ToString(),
+                Text = x.Numero
+            });
+
+            ViewBag.Prop = Prop;
+
             return View();
+        }
+
+        private List<ListPropiedad> CargarGrilla()
+        {
+            var propList = new List<ListPropiedad>();
+            using (var dt = BaseDatos.ExecuteDataTable("sp_c_list_propiedad"))
+            {
+                foreach (DataRow row in dt.Rows)
+                {
+                    var prop = new ListPropiedad()
+                    {
+                        Id = (int)row["id"],
+                        Numero = row["numero"].ToString(),
+                    };
+                    propList.Add(prop);
+                }
+            }
+            return propList;
+        }
+
+        [HttpPost]
+        public ActionResult Crear(Reserva cop)
+        {
+            var res = Guardar(cop);
+
+            if (res.Contains("Error"))
+            {
+                ViewBag.Tipo = 1;
+            }
+            else
+            {
+                ViewBag.Tipo = 0;
+            }
+
+            ViewBag.Message = res;
+
+            return View();
+        }
+        private String Guardar(Reserva cop)
+        {
+            try
+            {
+                BaseDatos.ExecuteSql("sp_i_registro_copropietario", cop.Propiedad, cop.Fecha);
+                return "Registro guardado";
+            }
+            catch (Exception ex)
+            {
+                return "Error: " + ex.Message;
+            }
         }
     }
 }
---
+++ CondominioWeb/Models/Reserva.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CondominioWeb.Models
+{
+    public class Reserva
+    {
+        public int Id { get; set; }
+        public string Propiedad {get;set;}
+        public string Fecha { get; set; }
+    }
+}
